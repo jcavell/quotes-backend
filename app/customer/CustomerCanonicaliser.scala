86a9931c96fd2f994081e740d65d6c6b154e343a
Jonny Cavell <jonny.cavell@digital.homeoffice.gov.uk>
2017-11-06 21:50:35 +0000

    Clean customer canonicaliser

--- app/customer/CustomerCanonicaliser.scala
+++ app/customer/CustomerCanonicaliser.scala
@@ -3,18 +3,12 @@ package customer
 object CustomerCanonicaliser {
 
   def canonicaliseName(input: String) = {
-
-    val lowercased = input.toLowerCase
-    val withoutTitle = lowercased.replaceAll("^mrs\\.? ", "").replaceAll("^mr\\.? ", "").replaceAll("^ms\\.? ", "").replaceAll("^miss\\.? ", "").replaceAll("^dr\\.? ", "")
+    val withoutTitle = input.toLowerCase.replaceAll("^mrs\\.? ", "").replaceAll("^mr\\.? ", "").replaceAll("^ms\\.? ", "").replaceAll("^miss\\.? ", "").replaceAll("^dr\\.? ", "")
     val withoutPunctuation = withoutTitle.replaceAll("[^a-z0-9]", "")
     withoutPunctuation
   }
 
-  def canonicaliseEmail(input: String) = {
-    input.toLowerCase.replaceAll("\\s", "")
-  }
+  def canonicaliseEmail(input: String) = input.toLowerCase.replaceAll("\\s", "")
 
-  def canonicaliseMobile(input: String) = {
-    input.replaceAll("\\D", "")
-  }
+  def canonicaliseMobile(input: String) = input.replaceAll("\\D", "")
 }
